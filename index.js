00cf3c7a3d5f5220408245e7a5a45ad959cf37bd
Muhammad Awais <awaisiqbal78692110@gmail.com>
2023-01-21 02:48:54 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,51 @@
-// const { MongoClient } = require("mongodb");
+const { MongoClient } = require("mongodb");
+// *============or=======================
+// const mongoClient = require("mongodb").MongoClient;
 
-// // *============or=======================
-
-// // const mongoClient = require("mongodb").MongoClient;
-
-
-// const url = "mongodb://localhost:27017";
-// const dataBase = "e-comm";
-// const client = new MongoClient(url);
+const url = "mongodb+srv://admin:admin@cluster0.dvpabex.mongodb.net/test";
+const dataBase = "e-comm";
+const client = new MongoClient(url);
 
 
+async function getData() {
+    let result = await client.connect();
+    let db = result.db(dataBase);
+    let collection = db.collection("products");
+    let response = await collection.find({}).toArray();
+    console.log(response);
+}
 
-// async function getData() {
-//     let result = await client.connect();
-//     let db = result.db(dataBase);
-//     let collection = db.collection("products");
-//     let response = await collection.find({}).toArray();
-//     console.log(response);
-// }
-
-// getData();
+getData();
 
 
 
 
 
-const { MongoClient } = require('mongodb');
+// const { MongoClient } = require('mongodb');
 
-// Connect to MongoDB
-const uri = "mongodb+srv://admin:admin@cluster0.dvpabex.mongodb.net/test";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-    if (err) {
-        console.log("Error connecting to MongoDB: ", err);
-    } else {
-        console.log("Connected to MongoDB!");
+// // Connect to MongoDB
+// const uri = "";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//     if (err) {
+//         console.log("Error connecting to MongoDB: ", err);
+//     } else {
+//         console.log("Connected to MongoDB!");
 
-        // Select the database and collection
-        const db = client.db("e-comm");
-        const collection = db.collection("products");
+//         // Select the database and collection
+//         const db = client.db("e-comm");
+//         const collection = db.collection("products");
 
-        // Retrieve all documents in the collection
-        collection.find({}).toArray((err, documents) => {
-            if (err) {
-                console.log("Error retrieving documents: ", err);
-            } else {
-                console.log("Documents: ", documents);
+//         // Retrieve all documents in the collection
+//         collection.find({}).toArray((err, documents) => {
+//             if (err) {
+//                 console.log("Error retrieving documents: ", err);
+//             } else {
+//                 console.log("Documents: ", documents);
 
-                // Close the connection
-                client.close();
-            }
-        });
-    }
-});
+//                 // Close the connection
+//                 client.close();
+//             }
+//         });
+//     }
+// });
