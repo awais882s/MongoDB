a514d22c6c7d4c25fedd7e31aa012c771caf1006
Muhammad Awais <awaisiqbal78692110@gmail.com>
2023-01-20 23:13:13 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const { mongoClient } = require("mongodb");
 // *============or=======================
 
+// const mongoClient = require("mongodb").MongoClient;
 
-const mongoClient = require("mongodb").MongoClient();
+
+const url = "mongodb://localhost:27017";
+const dataBase = "e-comm";
+const client = new mongoClient(url);
+
+
+
