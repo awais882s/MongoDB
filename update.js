f5ba98c7758c378b6b764123e9c517cc0efd7f7a
Muhammad Awais <awaisiqbal78692110@gmail.com>
2023-01-22 01:02:00 +0500

    multiple data update

--- update.js
+++ update.js
@@ -2,9 +2,9 @@ const dbConnect = require("./mongodb");
 
 const update = async () => {
     let db = await dbConnect();
-    let result = await db.updateOne(
-        { name: "HeartBeat" },
-        { $set: { name: "Soni Chasmato love", rollno: 883 } }
+    let result = await db.updateMany(
+        { name: "Muhammad Awais s" },
+        { $set: { name: "Soni Chasmato lovely update data", rollno: 883 } }
     )
     console.log(result);
 }
