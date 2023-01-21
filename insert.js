a7c14d6f2e31996c91bfc8126129068d22f036b2
Muhammad Awais <awaisiqbal78692110@gmail.com>
2023-01-21 23:51:18 +0500

    Update insert.js

--- insert.js
+++ insert.js
@@ -2,9 +2,15 @@ const dbConnect = require("./mongodb");
 
 const insert = async () => {
     const db = await dbConnect();
-    let result = await db.insertOne(
-        { name: "Muhammad Awais s", city: "Arifwala", rollno: 15424, role: "engineer" }
+    let result = await db.insertMany(
+        [
+            { name: "Muhammad Awais s", city: "Arifwala", rollno: 15424, role: "engineer" },
+            { name: "Soni Chasmato", city: "Arifwala", rollno: 882, role: "Lovely" }
+        ]
     );
+    if (result.acknowledged) {
+        console.log("Data is insertd");
+    }
     console.log(result)
 
 }
