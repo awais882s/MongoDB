02150c2722a50a3ac51fbf7c950982774d2c6444
Muhammad Awais <awaisiqbal78692110@gmail.com>
2023-01-22 15:36:18 +0500

    express js

--- api.js
+++ api.js
@@ -1,1 +1,9 @@
 const express = require("express");
+const dbConect = require("./mongodb");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send({ name: "awais" });
+})
+
+app.listen(4000);
