eb00f6e714575208ee5e71d8ec8fbea900148371
Muhammad Awais <awaisiqbal78692110@gmail.com>
2023-01-22 15:57:08 +0500

    Update api.js

--- api.js
+++ api.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const dbConect = require("./mongodb");
 const app = express();
-
+// get method to get data from database
 app.get("/", async (req, res) => {
     let data = await dbConect();
     let result = await data.find({}).toArray();
     console.log(result);
     res.send(result);
 })
+// post methods is use to save data in database
 
+app.post("/", (req, res) => {
+    res.send({ name: "awais" });
+});
 app.listen(4000);
